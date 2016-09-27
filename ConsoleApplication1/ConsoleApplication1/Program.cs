0ed995cd4bb3c8847652de0f8b8b1d9859e0a8de
jasmi <jasmi@DESKTOP-T3PI036>
2016-09-27 16:03:44 -0400

    input FizzBuzz

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,32 +10,59 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            //string y;
+            string y;
 
-            //Console.WriteLine("Pick # 1 - 100 to play FizzBuzz");
+            Console.WriteLine("Pick # 1 - 100 to play FizzBuzz");
 
-            //y = Console.ReadLine();
+            y = Console.ReadLine();
 
             int newNum;
-            for (int i = 0; i < 100; i++)
+
+            if (!Int32.TryParse(y, out newNum))
             {
-                if (i % 3 == 0 && i % 5 == 0)
-                {
-                    Console.WriteLine("FizzBuzz");
-                }
-                else if (i % 3 == 0)
-                {
-                    Console.WriteLine("Fizz");
-                }
-                else if (i % 5 == 0)
-                {
-                    Console.WriteLine("Buzz");
-                }
-                else
-                {
-                    Console.WriteLine(i);
-                }
+                Console.WriteLine("You didn't enter a #");
             }
+            else if (newNum % 3 == 0 && newNum % 5 ==0)
+            {
+                Console.WriteLine("FizzBuzz");
+            }
+            else if (newNum % 3 == 0)
+            {
+                Console.WriteLine("Fizz");
+            }
+            else if (newNum % 5 == 0)
+            {
+                Console.WriteLine("Buzz");
+            }
+            else
+            {
+                Console.WriteLine("Game Over");
+            }
+
+
+
+
+
+
+            //for (int i = 0; i < 100; i++)
+            //{
+            //    if (i % 3 == 0 && i % 5 == 0)
+            //    {
+            //        Console.WriteLine("FizzBuzz");
+            //    }
+            //    else if (i % 3 == 0)
+            //    {
+            //        Console.WriteLine("Fizz");
+            //    }
+            //    else if (i % 5 == 0)
+            //    {
+            //        Console.WriteLine("Buzz");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
 
             Console.ReadLine();
         }
